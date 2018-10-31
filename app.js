b7915368c4df867071f2cf619e61aac70409d2e7
Justin Tenpenny <justin.tenpenny@honeywell.com>
2018-10-31 15:43:16 -0400

    Initial commit

---
+++ app.js
@@ -0,0 +1,17 @@
+const http = require('http');
+const fs = require('fs');
+
+const hostname = 'localhost';
+const port = 8080;
+
+
+
+const server = http.createServer((req, res) => {
+    res.statusCode = 200;
+    res.setHeader('Content-Type', 'text/plain');
+    res.end('Hello World\n');
+});
+
+server.listen(port, hostname, () => {
+    console.log(`Server running at http://${hostname}:${port}/`);
+});
